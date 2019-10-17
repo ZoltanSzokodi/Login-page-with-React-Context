e560d2eef7bb24f7f94e82e54d04440a40cda814
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-10-17 15:54:37 +0200

    Enter the higher order component

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -5,18 +5,26 @@ export const LanguageContext = createContext();
 export class LanguageProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = { language: "french" };
+    this.state = { language: "spanish" };
     this.changeLanguage = this.changeLanguage.bind(this);
   }
   changeLanguage(e) {
-    this.setState({ language: e.target.value })
+    this.setState({ language: e.target.value });
   }
 
   render() {
     return (
-      <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>
+      <LanguageContext.Provider
+        value={{ ...this.state, changeLanguage: this.changeLanguage }}
+      >
         {this.props.children}
       </LanguageContext.Provider>
-    )
+    );
   }
 }
+
+export const withLanguageContext = Component => props => (
+  <LanguageContext.Consumer>
+    {value => <Component languageContext={value} {...props} />}
+  </LanguageContext.Consumer>
+);
